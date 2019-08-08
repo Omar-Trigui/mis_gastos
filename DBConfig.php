39abce926507defa0a89149cd852c6eec9e7b48c
Omar <Omar@DESKTOP-T0PFLV5>
2019-08-08 14:11:01 +0100

    first commit

---
+++ DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+ 
+//Define your host here.
+$HostName = "192.168.1.72";
+ 
+//Define your database name here.
+$DatabaseName = "misGastos";
+ 
+//Define your database username here.
+$HostUser = "root";
+ 
+//Define your database password here.
+$HostPass = "";
+ 
+?>
